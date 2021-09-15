d14f0c0b8cb59fb3cf2df6bd509b71a003a623d7
MW <matthewrhyswilliams@live.com>
2021-09-15 23:11:32 +0100

    Blockchain program returns string and hash value

--- src/main/java/BlockchainProgram.java
+++ src/main/java/BlockchainProgram.java
@@ -3,7 +3,10 @@ public class BlockchainProgram {
     public static void main(String[] args) {
 
         String statement1 = "it is wednesday my dudes";
+        int hashvalue = statement1.hashCode(); // this is built into java
 
+        System.out.println("Statement = " + statement1);
+        System.out.println("Hash value = " + hashvalue);
     }
 
 }
